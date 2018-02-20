8b9b84f21937d4387894e90a36c1a39c23f69c7f
GuyOhayon <GuyOhayon@users.noreply.github.com>
2018-02-20 17:30:30 +0200

    f

--- PTM1-2018-project/src/server/ISearchable.java
+++ PTM1-2018-project/src/server/ISearchable.java
@@ -1,11 +1,12 @@
 package server;
 
+import java.util.Collection;
 import java.util.List;
 
 public interface ISearchable<T> {
 	//according to the PPT from class
 	public State<T> getInitialState();
 	public boolean getGoalState(State<T> goalState);
-	public List<State<T>> getAllStates(State<T> allStates);
+	public Collection<State<PipeBoardGame>> getAllStates(State<T> allStates);
 	public String getStringUniqueId(String inputBuffer);
 }
